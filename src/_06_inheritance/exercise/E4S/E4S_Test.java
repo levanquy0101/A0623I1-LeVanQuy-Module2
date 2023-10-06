28230d717deff6924e254596a7775249e2b5c35d
levanquy0101 <quyle.01012002@gmail.com>
2023-10-06 13:45:46 +0700

    up

--- src/_06_inheritance/exercise/E4S/E4S_Test.java
+++ src/_06_inheritance/exercise/E4S/E4S_Test.java
@@ -2,7 +2,7 @@ package _06_inheritance.exercise.E4S;
 
 public class E4S_Test {
     public static void main(String[] args) {
-        E4S_Triangle triangle1 = new E4S_Triangle(3,7,9,"red");
+        E4S_Triangle triangle1 = new E4S_Triangle(3,7,8,"red");
         System.out.println("Màu sắc là: "+triangle1.getColor());
         System.out.println("Chu vi là: "+triangle1.getPerimeter());
         System.out.println("Diện tích là: "+triangle1.getArea());
